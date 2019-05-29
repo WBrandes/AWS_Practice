d3eba13d7244d885cd9cd3a0cd97aa1f1a772c22
WBrandes <brandesw@moravian.edu>
2019-05-29 14:34:50 -0400

    Changed text to an image element.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-	return("o==[:::::::::::>")
+	return("<img align:center;' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'/>")
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
