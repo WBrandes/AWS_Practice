ba6f85237763f868d266b53a8d962ff34be6a68b
WBrandes <brandesw@moravian.edu>
2019-05-29 15:22:42 -0400

    Replaced image with keycode display functionality

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-	return("<img src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'/>")
+	return render_template("home.html")
+	#return("<img src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'/>")
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
